4d3dba24d30743ce228880a57278202d8f08d212
Maryanne Muthoni <muthoni.w.maryanne@gmail.com>
2025-10-19 13:49:21 -0700

    script that prints the full description of the table books from the database alx_book_store in your MySQL server.
    The database name will be passed as an argument of the mysql command
    You are not allowed to use the DESCRIBE or EXPLAIN statements
    The name of the file should be task_4.sql
    All SQL keywords should be in uppercase

---
+++ task_4.sql
@@ -0,0 +1,17 @@
+-- script that prints the full description of the table books from the database alx_book_store in your MySQL server
+
+USE alx_book_store;
+
+SELECT
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM 
+	INFORMATION_SCHEMA.COLUMNS
+WHERE 
+	TABLE_SCHEMA = 'alx_book_store'
+	AND TABLE_NAME = 'books';
+
