83bf29306acc0ce39fa50f7eb61344ebe1d60a44
Maryanne Muthoni <muthoni.w.maryanne@gmail.com>
2025-10-19 13:21:00 -0700

    tasked with designing a MySQL database for an online bookstore
    Database Name: alx_book_store
    
    Books: Stores information about books available in the bookstore.
    
    book_id (Primary Key)
    title VARCHAR(130)
    author_id (Foreign Key referencing Authors table)
    price DOUBLE
    publication_date DATE
    Authors: Stores information about authors.
    
    author_id (Primary Key)
    author_name VARCHAR(215)
    Customers: Stores information about customers.
    
    customer_id (Primary Key)
    customer_name VARCHAR(215)
    email VARCHAR(215)
    address TEXT
    Orders: Stores information about orders placed by customers.
    
    order_id (Primary Key)
    customer_id (Foreign Key referencing Customers table)
    order_date DATE
    Order_Details: Stores information about the books included in each order.
    
    orderdetailid (Primary Key)
    order_id (Foreign Key referencing Orders table)
    book_id (Foreign Key referencing Books table)
    quantity DOUBLE
    NOTE : - The file extension should be alx_book_store.sql file - All SQL keywords should be in uppercase

---
+++ alx_book_store.sql
@@ -0,0 +1,40 @@
+-- Create MySQL database for an online bookstore
+
+CREATE DATABASE alx_book_store;
+
+CREATE TABLE Books(
+	book_id INTEGER PRIMARY KEY,
+	title VARCHAR(130),
+	author_id INTEGER,
+	price DOUBLE,
+	publication_date DATE,
+	FOREIGN KEY(author_id) REFRENCES Authors(author_id)
+);
+
+CREATE TABLE Authors(
+	author_id INTEGER PRIMARY KEY,
+	author_name VARCHAR(215)
+);
+
+CREATE TABLE Customers(
+	customer_id INTEGER PRIMARY KEY,
+	customer_name VARCHAR(215),
+	email VARCHAR(215),
+	address TEXT
+);
+
+CREATE TABLE Orders(
+	order_id INTEGER PRIMARY KEY,
+	customer_id INTEGER,
+	order_date DATE,
+	FOREIGN KEY(customer_id) REFRENCES Customers(customer_id)
+);
+
+CREATE TABLE Order_Details(
+	orderdetailid INTEGER PRIMARY KEY,
+	order_id INTEGER,
+	book_id INTEGER,
+	quantity DOUBLE,
+	FOREIGN KEY(order_id) REFERENCES Orders(order_id),
+	FOREIGN KEY(book_id) REFRENCES Books(book_id)
+);
