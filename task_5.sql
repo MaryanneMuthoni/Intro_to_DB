ee1959becd0c43107277829e22f58d063293db4a
Maryanne Muthoni <muthoni.w.maryanne@gmail.com>
2025-10-19 13:55:57 -0700

    script that inserts a single row in the table customer (database alx_book_store) in your MySQL server.
    Singel data insertion
    
    customer_id = 1
    customer_name = Cole Baidoo
    email = cbaidoo@sandtech.com
    address = 123 Happiness Ave.
    The database name will be passed as an argument of the mysql command
    
    Name of the file should be task_5.sql
    
    All SQL keywords should be in uppercase

---
+++ task_5.sql
@@ -0,0 +1,11 @@
+-- script that inserts a single row in the table customer (database alx_book_store) in your MySQL server
+
+USE DATABASE alx_book_store;
+
+INSERT INTO TABLE customer(
+	customer_id,
+	customer_name,
+	email,
+	address
+)
+VALUES(1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.");
