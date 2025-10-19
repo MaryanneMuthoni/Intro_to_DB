956e565c5f1cad13cc13b0b1ad4c45f70aa8f932
Maryanne Muthoni <muthoni.w.maryanne@gmail.com>
2025-10-19 13:27:38 -0700

    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id) fix

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -8,7 +8,7 @@ CREATE TABLE Books(
 	author_id INTEGER,
 	price DOUBLE,
 	publication_date DATE,
-	FOREIGN KEY(author_id) REFERENCES Authors(author_id)
+	FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
 CREATE TABLE Authors(
@@ -27,7 +27,7 @@ CREATE TABLE Orders(
 	order_id INTEGER PRIMARY KEY,
 	customer_id INTEGER,
 	order_date DATE,
-	FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
+	FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE Order_Details(
@@ -35,6 +35,6 @@ CREATE TABLE Order_Details(
 	order_id INTEGER,
 	book_id INTEGER,
 	quantity DOUBLE,
-	FOREIGN KEY(order_id) REFERENCES Orders(order_id),
-	FOREIGN KEY(book_id) REFRENCES Books(book_id)
+	FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+	FOREIGN KEY (book_id) REFRENCES Books(book_id)
 );
